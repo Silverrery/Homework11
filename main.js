2ed9db5b00ee3c8ebd41dad8b4f084a647c385ed
Silverrery <122677350+Silverrery@users.noreply.github.com>
2023-12-26 20:22:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+function Counter({ item: { id, number }, hdlUpdate, hdlRemove }) {
+  return (
+    <div className="counter">
+      <button onClick={() => hdlUpdate(id, -1)}> - </button>
+      <h3>{number}</h3>
+      <button onClick={() => hdlUpdate(id, 1)}> + </button>
+      <button onClick={() => hdlUpdate(id, -number)}> C </button>
+      <button onClick={() => hdlRemove(id)}> X </button>
+    </div>
+  );
+}
+
+function SumInfo(props) {
+    const stTitle = {
+      color: props.color,
+      fontSize: props.size === "big" ? "50px" : "40px",
+    };
+    return (
+      <div className="suminfo">
+        <h1 style={{ color: props.color, fontSize: "50px" }}>Sum = {props.sum}</h1>
+      </div>
+    );
+  }
+
+function App() {
+  const [counters, setCounters] = React.useState([{ id: 1, number: 0 }]);
+  const [sum, setSum] = React.useState(0);
+  const getTotal = () => {
+    return counters.reduce((acc, curr) => acc + curr.number, 0);
+  }
+
+
+  const hdlUpdate = (id, num) => {
+    const cloneCounters = [...counters];
+    let idx = cloneCounters.findIndex((el) => el.id === id);
+    if (cloneCounters[idx].number + num < 0) {
+      return;
+    }
+    cloneCounters[idx].number += num;
+    setCounters(cloneCounters);
+    updateSum(cloneCounters);
+  };
+
+  const hdlAddCounter = () => {
+    let newId = counters.length === 0 ? 1 : counters.at(-1).id + 1;
+
+    const cloneCounters = [...counters];
+    cloneCounters.push({ id: newId, number: 0 });
+    setCounters(cloneCounters);
+    updateSum(cloneCounters);
+  };
+
+  const hdlRemoveCounter = (id) => {
+    const cloneCounters = counters.filter((counter) => counter.id !== id);
+    setCounters(cloneCounters);
+    updateSum(cloneCounters);
+  };
+
+  const updateSum = (counters) => {
+    const totalSum = counters.reduce((sum, counter) => sum + counter.number, 0);
+    setSum(totalSum);
+  };
+
+  return (
+    <>
+      <h1 className="text-center">Codecamp Academy 01</h1>
+      <button className="text-center" onClick={hdlAddCounter}>
+        Add Counter
+      </button>
+      <SumInfo color="red" size="big" sum={sum} />
+      {counters.map((el) => (
+        <Counter
+          key={el.id}
+          item={el}
+          hdlUpdate={hdlUpdate}
+          hdlRemove={hdlRemoveCounter}
+        />
+      ))}
+    </>
+  );
+}
